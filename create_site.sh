dcc364e0bb25def4f96ec99b0e6245f946bbb7ab
Carl Wilson <carl@openpreservation.org>
2026-03-20 09:38:25 +0000

    FIX: Use script to build site.

--- create_site.sh
+++ create_site.sh
@@ -50,8 +50,8 @@ asciidoctor -a linkcss -a copycss -e -o - "$SCRIPT_DIR/guideline/E-ARK-guideline
 echo " - Generating guidelines PDF with asciidoctor."
 asciidoctor-pdf -o site/pdf/CITS-3DHM-GUIDELINES.pdf guideline/E-ARK-guideline-3DHM.adoc
 
-# echo " - Jekykll converting site for GitHub pages in _site directory."
-# docker run --rm -v "$PWD"/site:/usr/src/app -v "$PWD"/_site:/_site starefossen/github-pages jekyll build -d /_site
+echo " - Jekykll converting site for GitHub pages in _site directory."
+docker run --rm -v "$PWD"/site:/usr/src/app -v "$PWD"/_site:/_site starefossen/github-pages jekyll build -d /_site
 
 echo " - Cleaning up specification and site directories."
 git clean -f "$SCRIPT_DIR/specification/"
