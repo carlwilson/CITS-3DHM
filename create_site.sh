e4e105f88e250a5ce68f15431676420415b47cd3
Carl Wilson <carl@openpreservation.org>
2026-04-24 17:17:00 +0100

    FIX: Publication issues
    - added extra cleanup at the start and end of `create_site.sh` to ensure no cruft is left in the `site/` directory;
    - removed redundant `$SCRIPT_DIR` references in `create_site.sh`;
    - added root requirements table to `specification/E-ARK-CITS-3DHM.adoc`;
    - replaced "Product Model" type references in the profile examples; and
    - updated `spec-publisher` to 0.2.0-SNAPSHOT to include the latest fixes for appendix and requirements serialisation.

--- create_site.sh
+++ create_site.sh
@@ -4,48 +4,52 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR" || exit
 
 echo " - Cleaning up site directory and copying spec-publisher site..."
-git clean -f "$SCRIPT_DIR/specification/"
-git clean -f "$SCRIPT_DIR/site/"
+git clean -f "specification/"
+git clean -f "guideline/"
+git clean -f "site/"
+git checkout "site/index.html"
+git checkout "site/guidelines/index.html"
 
 if [ -d _site ]
 then
   echo " - Removing old _site directory contents"
-  rm -rf "$SCRIPT_DIR/_site/*"
+  rm -rf "_site/*"
 fi
 
 echo " - copying files to site directory..."
 # Copy spec-publisher artifacts to the site
-cp -rf "$SCRIPT_DIR/spec-publisher/site/"* "$SCRIPT_DIR/spec-publisher/res/md/figs" "$SCRIPT_DIR/site/"
+cp -rf "spec-publisher/site/"* "spec-publisher/res/md/figs" "site/"
 # Copy remaining project collaterel to the site
-cp -rf "$SCRIPT_DIR/profile" "$SCRIPT_DIR/examples" "$SCRIPT_DIR/schema" "$SCRIPT_DIR/specification/figs" "$SCRIPT_DIR/pdf" "$SCRIPT_DIR/site/"
-cp -rf "$SCRIPT_DIR/guideline/figs" "$SCRIPT_DIR/spec-publisher/res/md/figs" "$SCRIPT_DIR/site/guidelines/"
+cp -rf "profile" "examples" "schema" "specification/figs" "pdf" "site/"
+cp -rf "guideline/figs" "spec-publisher/res/md/figs" "site/guidelines/"
 
 echo " - spec-publisher: generating specification requirement tables, appendices, etc."
 mvn package -f spec-publisher/pom.xml 
-java -jar "$SCRIPT_DIR/spec-publisher/target/mets-profile-processor-0.2.0-SNAPSHOT.jar" \
-     -f "$SCRIPT_DIR/specification.yaml" \
-     -o "$SCRIPT_DIR/specification" \
-     profile/E-ARK-3DHM-ROOT_v1.0.0.xml "profile/E-ARK-3DHM-REPRESENTATION_v1.0.0.xml"
+java -jar "spec-publisher/target/mets-profile-processor-0.2.0-SNAPSHOT.jar" \
+     -f "specification.yaml" \
+     -o "specification" \
+     "profile/E-ARK-3DHM-ROOT_v1.0.0.xml" "profile/E-ARK-3DHM-REPRESENTATION_v1.0.0.xml"
 
 echo " - Copying spec-publisher collateral to specification directory."
-cp -rf "$SCRIPT_DIR/spec-publisher/res/md/common-intro.adoc" "$SCRIPT_DIR/spec-publisher/res/md/figs" "$SCRIPT_DIR/specification/"
+cp -rf "spec-publisher/res/md/common-intro.adoc" "spec-publisher/res/md/figs" "specification/"
 
 echo " - Generating specification HTML with asciidoctor."
-asciidoctor -a linkcss -a copycss -e -o - "$SCRIPT_DIR/specification/E-ARK-CITS-3DHM.adoc" >> "$SCRIPT_DIR/site/index.html"
+asciidoctor -a linkcss -a copycss -e -o - "specification/E-ARK-CITS-3DHM.adoc" >> "site/index.html"
 
 echo " - Generating specification PDF with asciidoctor."
 asciidoctor-pdf -o site/pdf/E-ARK-CITS-3DHM.pdf specification/E-ARK-CITS-3DHM.adoc
 
 echo " - Copying spec-publisher collateral to guidelines directory."
-cp -rf "$SCRIPT_DIR/spec-publisher/res/md/common-intro.adoc" "$SCRIPT_DIR/spec-publisher/res/md/figs" "$SCRIPT_DIR/guideline/"
+cp -rf "spec-publisher/res/md/common-intro.adoc" "spec-publisher/res/md/figs" "guideline/"
 
 echo " - spec-publisher: generating guidelines, appendices, etc."
-java -jar "$SCRIPT_DIR/spec-publisher/target/mets-profile-processor-0.2.0-SNAPSHOT.jar" \
-     -f "$SCRIPT_DIR/guideline.yaml" \
-     -o "$SCRIPT_DIR/guideline"
+java -jar "spec-publisher/target/mets-profile-processor-0.2.0-SNAPSHOT.jar" \
+     -f "guideline.yaml" \
+     -o "guideline" \
+     "profile/E-ARK-3DHM-ROOT_v1.0.0.xml"
 
 echo " - Generating guidelines HTML with asciidoctor."
-asciidoctor -a linkcss -a copycss -e -o - "$SCRIPT_DIR/guideline/E-ARK-guideline-3DHM.adoc" >> "$SCRIPT_DIR/site/guidelines/index.html"
+asciidoctor -a linkcss -a copycss -e -o - "guideline/E-ARK-guideline-3DHM.adoc" >> "site/guidelines/index.html"
 
 echo " - Generating guidelines PDF with asciidoctor."
 asciidoctor-pdf -o site/pdf/CITS-3DHM-GUIDELINES.pdf guideline/E-ARK-guideline-3DHM.adoc
@@ -54,5 +58,6 @@ echo " - Jekykll converting site for GitHub pages in _site directory."
 docker run --rm -v "$PWD"/site:/usr/src/app -v "$PWD"/_site:/_site starefossen/github-pages jekyll build -d /_site
 
 echo " - Cleaning up specification and site directories."
-git clean -f "$SCRIPT_DIR/specification/"
-git clean -f "$SCRIPT_DIR/site/"
+git clean -f "specification/"
+git clean -f "guideline/"
+git clean -f "site/"
